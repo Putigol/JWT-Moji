7b2bc0285c3ed90b10d9a256f5cf7a234c2c1aa6
Nguyen Phuc Thinh <23521504@gm.uit.edu.vn>
2026-08-04 11:53:08 +0700

    Auth Middleware

---
+++ backend/src/controllers/userController.js
@@ -0,0 +1,11 @@
+export const authMe = async (req, res) => {
+  try {
+    const user = req.user; // Lấy thông tin người dùng từ authMiddleware
+    return res.status(200).json({ user });
+  } catch (error) {
+    console.error("Lỗi khi gọi authMe:", error);
+    res
+      .status(500)
+      .json({ message: "Có lỗi hệ thống xảy ra khi lấy thông tin user" });
+  }
+};
---
+++ backend/src/middlewares/authMiddleware.js
@@ -0,0 +1,43 @@
+import jwt from "jsonwebtoken";
+import User from "../models/User.js";
+
+//authorization: xác minh user là ai
+//next: chuyển sang middleware tiếp theo
+export const protectedRoute = async (req, res, next) => {
+  try {
+    //lấy token từ header
+    const authHeader = req.headers["authorization"];
+    const token = authHeader && authHeader.split(" ")[1]; //tách: Bearer <token>
+
+    if (!token) {
+      return res.status(401).json({ message: "Không tìm thấy access token" });
+    }
+    //xác nhận token hợp lệ
+    jwt.verify(
+      token,
+      process.env.ACCESS_TOKEN_SECRET,
+      async (err, decodedUser) => {
+        if (err) {
+          console.error("Lỗi khi xác thực token:", err);
+          return res
+            .status(403)
+            .json({ message: "Access token hết hạn hoặc không đúng" });
+        }
+
+        //tìm user
+        const user = await User.findById(decodedUser.userId).select(
+          "-hashedPassword",
+        ); //loại bỏ hashedPassword khỏi dữ liệu trả về
+
+        if (!user) {
+          return res.status(404).json({ message: "Không tìm thấy user" });
+        }
+        //trả user về trong req
+        req.user = user;
+        next();
+      },
+    );
+  } catch (error) {
+    return res.status(401).json({ message: "Lỗi hệt thống khi xác thực" });
+  }
+};
