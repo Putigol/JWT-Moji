2cb9086f73c9ff55e670c55d20a24ddd983a264b
Nguyen Phuc Thinh <23521504@gm.uit.edu.vn>
2026-08-04 15:36:36 +0700

    Setup Frontend with React+Vite

---
+++ frontend/src/App.tsx
@@ -0,0 +1,26 @@
+import { BrowserRouter, Route, Routes } from "react-router";
+import SignInPage from "./pages/SignInPage";
+import ChatAppPage from "./pages/ChatAppPage";
+import { Toaster } from "sonner";
+import SignUpPage from "./pages/SignUpPage";
+
+function App() {
+  return (
+    <>
+      <Toaster richColors />
+      <BrowserRouter>
+        <Routes>
+          {/* public routes */}
+          <Route path="/signin" element={<SignInPage />} />
+          <Route path="/signup" element={<SignUpPage />} />
+
+          {/* protected routes */}
+          {/* todo: tạo protected routes */}
+          <Route path="/" element={<ChatAppPage />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
