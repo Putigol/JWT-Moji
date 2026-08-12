bef64e5592af035024ccc5fac25d281d4b72c541
Nguyen Phuc Thinh <23521504@gm.uit.edu.vn>
2026-08-12 16:01:54 +0700

    Message APIs

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,6 +6,7 @@ import userRoute from "./routes/userRoute.js";
 import cookieParser from "cookie-parser";
 import { protectedRoute } from "./middlewares/authMiddleware.js";
 import friendRoute from "./routes/friendRoute.js";
+import messageRoute from "./routes/messageRoute.js";
 import cors from "cors";
 
 dotenv.config();
@@ -25,6 +26,7 @@ app.use("/api/auth", authRoute);
 app.use(protectedRoute);
 app.use("/api/users", userRoute);
 app.use("/api/friends", friendRoute);
+app.use("/api/messages", messageRoute);
 
 connectDB().then(() => {
   app.listen(PORT, () => {
---
+++ backend/src/utils/messageHelper.js
@@ -0,0 +1,26 @@
+//Chứa các helper liên quan đến controller
+export const updateConversationAfterCreateMessage = (
+  conversation,
+  message,
+  senderId,
+) => {
+  //Khi 1 message được gửi thì cần reset seenBy, cập nhật lastMessage
+  conversation.set({
+    seenBy: [],
+    lastMessageAt: message.createdAt,
+    lastMessage: {
+      _id: message._id,
+      content: message.content,
+      senderId,
+      createdAt: message.createdAt,
+    },
+  });
+
+  //Khi có message mới thì reset tin chưa đọc (sender=0, receiver+=1)
+  conversation.participants.forEach((p) => {
+    const memberId = p.userId.toString();
+    const isSender = memberId === senderId.toString();
+    const prevCount = conversation.unreadCounts.get(memberId) || 0;
+    conversation.unreadCounts.set(memberId, isSender ? 0 : prevCount + 1);
+  });
+};
