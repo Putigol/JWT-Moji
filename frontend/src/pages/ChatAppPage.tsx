d441bd84d60889f1694e6f31db2387e278cc18df
Nguyen Phuc Thinh <23521504@gm.uit.edu.vn>
2026-08-15 23:45:56 +0700

    Sidebar's blocks from shadcn

--- frontend/src/pages/ChatAppPage.tsx
+++ frontend/src/pages/ChatAppPage.tsx
@@ -1,29 +1,13 @@
-import { useAuthStore } from "@/stores/useAuthStore";
-import Logout from "../components/auth/Logout";
-import api from "@/lib/axios";
-import { Button } from "@/components/ui/button";
-import { toast } from "sonner";
+import { AppSidebar } from "@/components/sidebar/app-sidebar";
+import { SidebarProvider } from "@/components/ui/sidebar";
+
 const ChatAppPage = () => {
-  //theo dõi trường user trong store, chỉ render khi user thay đổi
-  const user = useAuthStore((s) => s.user);
-
-  const handleOnClick = async () => {
-    try {
-      await api.get("/users/test", { withCredentials: true });
-      toast.success("ok");
-    } catch (error) {
-      toast.error("Thất bại!");
-      console.error(error);
-    }
-  };
   return (
-    <div>
-      {/* nếu user có giá trị thì hiển thị username */}
-      {user?.username}
-      <Logout />
+    <SidebarProvider>
+      <AppSidebar />
 
-      <Button onClick={handleOnClick}>test</Button>
-    </div>
+      <div className="flex h-screen w-full p-2"></div>
+    </SidebarProvider>
   );
 };
 
