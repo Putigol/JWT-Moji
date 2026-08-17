fa74e52fdbd981b807f7f990d176be36e1fbe651
Nguyen Phuc Thinh <23521504@gm.uit.edu.vn>
2026-08-17 17:28:33 +0700

    fixed

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,8 +4,15 @@ import ChatAppPage from "./pages/ChatAppPage";
 import { Toaster } from "sonner";
 import SignUpPage from "./pages/SignUpPage";
 import ProtectedRoute from "./components/auth/ProtectedRoute";
+import { useThemeStore } from "./stores/useThemeStore";
+import { useEffect } from "react";
 
 function App() {
+  const { isDark, setTheme } = useThemeStore();
+
+  useEffect(() => {
+    setTheme(isDark);
+  }, [isDark, setTheme]);
   return (
     <>
       <Toaster richColors />
--- frontend/src/components/sidebar/app-sidebar.tsx
+++ frontend/src/components/sidebar/app-sidebar.tsx
@@ -20,14 +20,9 @@ import GroupChatList from "../chat/GroupChatList";
 import AddFriendModal from "../chat/AddFriendModal";
 import DirectMessageList from "../chat/DirectMessageList";
 import { useThemeStore } from "@/stores/useThemeStore";
-import { useEffect } from "react";
 
 export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
-  const { isDark, setTheme } = useThemeStore();
-
-  useEffect(() => {
-    setTheme(isDark);
-  }, [isDark, setTheme]);
+  const { isDark, toggleTheme } = useThemeStore();
 
   return (
     <Sidebar variant="inset" {...props}>
@@ -46,7 +41,7 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
                     <Sun className="w-4 h-4 text-white/90" />
                     <Switch
                       checked={isDark}
-                      onCheckedChange={setTheme}
+                      onCheckedChange={toggleTheme}
                       className="data-[state=checked]:bg-white/60"
                     />
                     <Moon className="w-4 h-4 text-white/90" />
