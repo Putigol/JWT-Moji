a8a9cd257866b7677ddf8e5294b8b4a9e1822293
Nguyen Phuc Thinh <23521504@gm.uit.edu.vn>
2026-08-18 16:54:13 +0700

    DirectMessageCard & GroupChatCard

--- frontend/src/components/chat/DirectMessageCard.tsx
+++ frontend/src/components/chat/DirectMessageCard.tsx
@@ -1,10 +1,71 @@
 import type { Conversation } from "@/types/chat";
 import ChatCard from "./ChatCard";
+import { useAuthStore } from "@/stores/useAuthStore";
+import { useChatStore } from "@/stores/useChatStore";
+import { cn } from "@/lib/utils";
 
 const DirectMessageCard = ({ convo }: { convo: Conversation }) => {
+  const { user } = useAuthStore(); //lấy thông tin user hiện tại
+  const {
+    activeConversationId, //biết conversation nào được chọn
+    setActiveConversation, //đổi activeConversation khi chọn conversation khác
+    messages,
+    // fetchMessages, //list tin nhắn
+  } = useChatStore();
+
+  if (!user) return null;
+
+  //tìm ra người mà user nói chuyện cùng
+  const otherUser = convo.participants.find((p) => p._id !== user._id);
+  if (!otherUser) return null;
+
+  //lấy số lượng tin nhắn chưa đọc của user
+  const unreadCount = convo.unreadCounts[user._id];
+  //lấy nội dụng message cuối cùng nếu có
+  const lastMessage = convo.lastMessage?.content ?? "";
+
+  //tạo 1 hàm xử lý khi user click vào 1 conversation
+  const handleSelectConversation = async (id: string) => {
+    setActiveConversation(id);
+    if (!messages[id]) {
+      // await fetchMessages(); //load message nếu conversation chưa có tin nhắn
+      //todo: fetch messages
+    }
+  };
   return (
     <div>
-      <ChatCard></ChatCard>
+      <ChatCard
+        convoId={convo._id}
+        name={otherUser.displayName ?? ""}
+        timestamp={
+          convo.lastMessage?.createdAt
+            ? new Date(convo.lastMessage.createdAt)
+            : undefined
+        }
+        isActive={activeConversationId === convo._id}
+        onSelect={handleSelectConversation}
+        unreadCount={unreadCount}
+        leftSection={
+          <>
+            {/* todo: avatar */}
+            {/* todo: status badge */}
+            {/* todo: unread count */}
+          </>
+        }
+        //trong direct chat thì subtitle là last message
+        subtitle={
+          <p
+            className={cn(
+              "text-sm truncate",
+              unreadCount > 0 //nếu còn unread thì làm đậm lên
+                ? "font-medium text-foreground"
+                : "text-muted-foreground",
+            )}
+          >
+            {lastMessage}
+          </p>
+        }
+      />
     </div>
   );
 };
