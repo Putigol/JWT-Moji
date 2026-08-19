ddad2b45db2240b6030072ef40b43d837172c45b
Nguyen Phuc Thinh <23521504@gm.uit.edu.vn>
2026-08-19 17:15:12 +0700

    ChatWindowHeader's UI

--- frontend/src/components/chat/ChatWindowHeader.tsx
+++ frontend/src/components/chat/ChatWindowHeader.tsx
@@ -1,5 +1,73 @@
-const ChatWindowHeader = () => {
-  return <div>ChatWindowHeader</div>;
+import { useAuthStore } from "@/stores/useAuthStore";
+import { useChatStore } from "@/stores/useChatStore";
+import type { Conversation } from "@/types/chat";
+import { SidebarTrigger } from "../ui/sidebar";
+import UserAvatar from "./UserAvatar";
+import StatusBadge from "./StatusBadge";
+import GroupChatAvatar from "./GroupChatAvatar";
+import { Separator } from "../ui/separator";
+
+const ChatWindowHeader = ({ chat }: { chat?: Conversation }) => {
+  const { conversations, activeConversationId } = useChatStore();
+  const { user } = useAuthStore();
+
+  let otherUser;
+
+  //nếu props chat chưa truyền vào thì tìm trong convo list có convo nào trùng với active ko
+  chat = chat ?? conversations.find((c) => c._id === activeConversationId);
+
+  //nếu chưa có chat thì chỉ cần hiển thị SidebarTrigger
+  if (!chat) {
+    return (
+      <header className="md:hidden sticky top-0 z-10 flex items-center gap-2 px-4 py-2 w-full">
+        <SidebarTrigger className="-ml-1 text-foreground" />
+      </header>
+    );
+  }
+
+  //
+  if (chat.type === "direct") {
+    const otherUsers = chat.participants.filter((p) => p._id !== user?._id);
+    otherUser = otherUsers.length > 0 ? otherUsers[0] : null;
+
+    if (!user || !otherUser) return;
+  }
+
+  return (
+    <header className="sticky top-0 z-10 px-4 py-2 flex items-center bg-background">
+      <div className="flex items-center gap-2 w-full">
+        <SidebarTrigger className="-ml-1 text-foreground" />
+        <Separator orientation="vertical" className="mr-2 h-4 w-px bg-border" />
+
+        <div className="p-2 w-full flex items-center gap-3">
+          {/* avatar */}
+          <div className="relative">
+            {chat.type === "direct" ? (
+              <>
+                <UserAvatar
+                  type={"sidebar"}
+                  name={otherUser?.displayName || "Moji"}
+                  avatarUrl={otherUser?.avatarUrl || undefined}
+                />
+                {/* todo: socket io */}
+                <StatusBadge status={"offline"} />
+              </>
+            ) : (
+              <GroupChatAvatar
+                participants={chat.participants}
+                type="sidebar"
+              />
+            )}
+          </div>
+
+          {/* name */}
+          <h2 className="font-semibold text-foreground">
+            {chat.type === "direct" ? otherUser?.displayName : chat.group?.name}
+          </h2>
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default ChatWindowHeader;
