175d3889d15b8d470dab088661f5ed8a07b48f3c
Nguyen Phuc Thinh <23521504@gm.uit.edu.vn>
2026-08-19 15:24:58 +0700

    ChatWindowLayout

--- frontend/src/components/chat/ChatWindowLayout.tsx
+++ frontend/src/components/chat/ChatWindowLayout.tsx
@@ -1,5 +1,39 @@
+import { useChatStore } from "@/stores/useChatStore";
+import ChatWelcomeScreen from "./ChatWelcomeScreen";
+import ChatWindowSkeleton from "./ChatWindowSkeleton";
+import { SidebarInset } from "../ui/sidebar";
+import ChatWindowHeader from "./ChatWindowHeader";
+import ChatWindowBody from "./ChatWindowBody";
+import MessageInput from "./MessageInput";
+
 const ChatWindowLayout = () => {
-  return <div>ChatWindowLayout</div>;
+  const {
+    activeConversationId,
+    conversations,
+    messageLoading: loading,
+  } = useChatStore();
+
+  //cần biết cuộc trò chuyện nào được chọn
+  const selectedConvo =
+    conversations.find((c) => c._id === activeConversationId) ?? null;
+
+  if (!selectedConvo) return <ChatWelcomeScreen />;
+
+  if (loading) return <ChatWindowSkeleton />;
+  return (
+    <SidebarInset className="flex flex-col h-full flex-1 overflow-hidden rounded-sm shadow-md">
+      {/* Header */}
+      <ChatWindowHeader />
+
+      {/* Body */}
+      <div className="flex-1 overflow-y-auto bg-primary-foreground">
+        <ChatWindowBody />
+      </div>
+
+      {/* Footer */}
+      <MessageInput />
+    </SidebarInset>
+  );
 };
 
 export default ChatWindowLayout;
---
+++ frontend/src/components/chat/ChatWindowSkeleton.tsx
@@ -0,0 +1,6 @@
+//Skeleton là khung mô phỏng nội dung (để user cảm giác trang vẫn đang load)
+const ChatWindowSkeleton = () => {
+  return <div>ChatWindowSkeleton</div>;
+};
+
+export default ChatWindowSkeleton;
