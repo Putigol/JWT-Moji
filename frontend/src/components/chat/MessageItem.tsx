28c2975504b5c466f5c941ac6c4ceaadbbb5b9dc
Nguyen Phuc Thinh <23521504@gm.uit.edu.vn>
2026-08-20 17:24:30 +0700

    MessageItem's UI

---
+++ frontend/src/components/chat/MessageItem.tsx
@@ -0,0 +1,104 @@
+import { cn, formatMessageTime } from "@/lib/utils";
+import type { Conversation, Message, Participant } from "@/types/chat";
+import { Badge } from "../ui/badge";
+import { Card } from "../ui/card";
+import UserAvatar from "./UserAvatar";
+
+interface MessageItemProps {
+  message: Message;
+  index: number; //thứ tự message trong list
+  messages: Message[];
+  selectedConvo: Conversation;
+  lastMessageStatus: "delivered" | "seen";
+}
+
+const MessageItem = ({
+  message,
+  index,
+  messages,
+  selectedConvo,
+  lastMessageStatus,
+}: MessageItemProps) => {
+  const prev = messages[index - 1];
+
+  //tạo flag quyết định xem có tách nhóm message ko
+  const isGroupBreak =
+    index === 0 ||
+    new Date(message.createdAt).getTime() -
+      new Date(prev?.createdAt || 0).getTime() >
+      300000; // 5 phút
+
+  //lấy data sender để hiển thị tên và avatar
+  const participant = selectedConvo.participants.find(
+    (p: Participant) => p._id.toString() === message.senderId.toString(),
+  );
+
+  return (
+    <>
+      <div
+        className={cn(
+          "flex gap-2 message-bounce mt-1",
+          message.isOwn ? "justify-end" : "justify-start", //của login user thì căn phải và ngược lại
+        )}
+      >
+        {/* avatar */}
+        {!message.isOwn && (
+          <div className="w-8">
+            {isGroupBreak && (
+              <UserAvatar
+                type="chat"
+                name={participant?.displayName ?? "Moji"}
+                avatarUrl={participant?.avatarUrl ?? undefined}
+              />
+            )}
+          </div>
+        )}
+
+        {/* tin nhắn */}
+        <div
+          className={cn(
+            "max-w-xs lg:max-w-md space-y-1 flex flex-col",
+            message.isOwn ? "items-end" : "items-start",
+          )}
+        >
+          <Card
+            className={cn(
+              "p-3",
+              message.isOwn
+                ? "chat-bubble-sent border-0"
+                : "chat-bubble-received",
+            )}
+          >
+            <p className="text-sm leading-relaxed break-words">
+              {message.content}
+            </p>
+          </Card>
+
+          {/* time */}
+          {isGroupBreak && (
+            <span className="text-xs text-muted-foreground px-1">
+              {formatMessageTime(new Date(message.createdAt))}
+            </span>
+          )}
+
+          {/* seen/ delivered */}
+          {message.isOwn && message._id === selectedConvo.lastMessage?._id && (
+            <Badge
+              variant="outline"
+              className={cn(
+                "text-xs px-1.5 py-0.5 h-4 border-0",
+                lastMessageStatus === "seen"
+                  ? "bg-primary/20 text-primary"
+                  : "bg-muted text-muted-foreground",
+              )}
+            >
+              {lastMessageStatus}
+            </Badge>
+          )}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default MessageItem;
