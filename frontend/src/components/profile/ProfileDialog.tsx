86da980ae27de2a629950ff30e1ed3568f59acd1
Nguyen Phuc Thinh <23521504@gm.uit.edu.vn>
2026-08-28 23:29:32 +0700

    User's profile

---
+++ frontend/src/components/profile/ProfileDialog.tsx
@@ -0,0 +1,32 @@
+import type { Dispatch, SetStateAction } from "react";
+import { Dialog, DialogContent, DialogHeader, DialogTitle } from "../ui/dialog";
+import ProfileCard from "./ProfileCard";
+import { useAuthStore } from "@/stores/useAuthStore";
+
+interface ProfileDialogProps {
+  open: boolean;
+  setOpen: Dispatch<SetStateAction<boolean>>;
+}
+const ProfileDialog = ({ open, setOpen }: ProfileDialogProps) => {
+  const { user } = useAuthStore();
+  return (
+    <Dialog open={open} onOpenChange={setOpen}>
+      <DialogContent className="overflow-y-auto max-h-[95vh] p-0 bg-transparent border-0 shadow-2xl">
+        <div className="bg-gradient-glass">
+          <div className="max-w-4xl mx-auto p-4">
+            {/* heading */}
+            <DialogHeader className="mb-6">
+              <DialogTitle className="text-2xl font-bold text-foreground">
+                Hồ sơ & Cài đặt
+              </DialogTitle>
+            </DialogHeader>
+          </div>
+        </div>
+
+        <ProfileCard user={user} />
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default ProfileDialog;
